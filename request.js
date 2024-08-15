a959e57f758084bde5fb262c5adc0e77e9299a0f
MuhammadZeshanLatif <zeeshanmuhammad42201@gmail.com>
2024-08-15 13:41:25 +0500

    Cheerio
    Cheerio give data

--- request.js
+++ request.js
@@ -1,8 +1,23 @@
-const request =require('request');
+const request = require('request');
+const cheerio = require('cheerio');
 
-const callBack=(error,response,body)=>{
-    console.log(`Error:${error}`);
-    console.log(`Response:${response && response.statusCode}`);
-    console.log(`body: ${body}`);
-}
-request('http://www.google.com',callBack);
+const callBack = (error, response, html) => {
+    console.log(`Response: ${response && response.statusCode}`);
+    if (error) {
+        console.log(`Error: ${error}`);
+    } else {
+        allData(html);
+    }
+};
+
+request('https://souq-today.com/en', callBack);
+
+const allData = (html) => {
+    const selTol = cheerio.load(html);
+    const souqData = selTol("td");
+
+    souqData.each((index, element) => {
+        const tdText = selTol(element).text().trim();
+        console.log(`TD ${index + 1}: ${tdText}`);
+    });
+};
